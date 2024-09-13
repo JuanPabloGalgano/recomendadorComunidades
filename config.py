0a6590c060329527921a76606bd2314685a0bdf0
Juan Pablo Galgano <jgalgano@frba.utn.edu.ar>
2024-09-13 16:34:15 -0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class DevelopmentConfig():
       DEBUG = True
       MYSQL_HOST = 'localhost'
       MYSQL_USER = 'root'
-      MYSQL_PASSWORD = 'Operator.2024'
+      MYSQL_PASSWORD = 'contraseñaGenerica'
       MYSQL_DB = 'instituciones_donantes'
       MYSQL_CURSORCLASS = 'DictCursor'
        
